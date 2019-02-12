ff47d81018fe7678f1fd855d97762215a3defe0c
V2 <v2@catchdesign.co.nz>
2019-02-12 15:08:38 +1300

    deps

--- _config.php
+++ _config.php
@@ -9,7 +9,7 @@ use Azt3k\SS\Social\Extensions\SocialUpdatePageExtension;
 use SilverStripe\Core\Config\Config;
 use Silverstripe\SiteConfig\SiteConfig;
 use SilverStripe\View\Parsers\ShortcodeParser;
-// use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorConfig;
 
 // Define path constant
 $path = str_replace('\\', '/', __DIR__);
@@ -28,7 +28,11 @@ InstagramUpdate::add_extension(SocialUpdatePageExtension::class);
 
 // add the embed functionality
 if (!Config::inst()->get('SocialGlobalConf', 'disable_wysiwyg_embed')) {
-    ShortcodeParser::get('default')->register('social_embed', SocialUpdatePageExtension::SocialEmbedParser);
+    //$this->shortcodes[$tag], $attributes, $content, $this, $tag, $extra);
+	}
+    ShortcodeParser::get('default')->register('social_embed', function($arguments, $content = null, $parser = null, $tagName){
+        return SocialUpdatePageExtension::SocialEmbedParser($arguments, $content, $parser, $tagName);
+    });
     HtmlEditorConfig::get('cms')->enablePlugins(array(
         'social_embed' => '../../../' . ABC_SOCIAL_DIR . '/js/editor-plugin.js'
     ));
